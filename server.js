f04e7aef218eb6274e43a4d5a4dec0455fedda90
Ltrain <layconmm@msn.com>
2022-04-14 22:08:42 -0500

    create Pin model

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const{ ApolloServer } = require('apollo-server')
+const{ ApolloServer } = require('apollo-server');
 
-const typeDefs = require('./typeDefs')
-const resolvers = require('./resolvers')
-// const mongoose = require('mongoose')
-// require('dotenv').config()
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-// mongoose
-// .connect(process.env.MONGO_URI, { useNewUrlParser: 
-// true})
-// .then(() => console.log('DB connected!'))
-// .catch(err => console.log(err))
+mongoose
+.connect(process.env.MONGO_URI, { useNewUrlParser: 
+true})
+.then(() => console.log('DB connected!'))
+.catch(err => console.log(err));
 
 const server = new ApolloServer ({
 typeDefs,
