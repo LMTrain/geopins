e4f1c204a2eaf55fc436e819139cb055fae742fb
Ltrain <layconmm@msn.com>
2022-04-18 20:48:53 -0500

    Change picture

--- resolvers.js
+++ resolvers.js
@@ -2,7 +2,7 @@ const user = {
     _id: "1",
     name: "Laycon",
     email: "layconmm@msn.com",
-    picture: "https://lmtrain.github.io/lm-images/assets/images/bugatti_1.jpg"
+    picture: "https://lmtrain.github.io/lm-images/assets/images/bugatti_2.jpg"
 }
 
 module.exports = {
