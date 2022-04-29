0147a4aa27d892b0c618b457bb49219ddba318d8
LMTrain <layconmm@msn.com>
2022-04-28 19:03:07 -0500

    Authenticating Users using Google OAuth

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { GoogleLogin } from 'react-google-login';
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  const onSuccess = googleUser => {
+    // console.log({ googleUser})
+    const idToken = googleUser.getAuthResponse().id_token;
+    console.log({ idToken })
+  }
+  return <GoogleLogin 
+  clientId="60379064216-odedg154ipn4rpsns5usfbl8obnpop5g.apps.googleusercontent.com"
+  onSuccess={onSuccess}
+  isSignedIn={true}
+  />
 };
 
 const styles = {
