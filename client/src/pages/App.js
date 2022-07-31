bcc3e0a063273fff4f921c030acfd9b8d78e855e
LMTrain <layconmm@msn.com>
2022-07-31 18:27:00 -0500

    Give tile to App page

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import withRoot from "../withRoot";
 
 const App = () => {
-  return <div>LM Geo Pins Apps</div>;
+  return <div>LM Systems Geo Pins Apps</div>;
 };
 
 export default withRoot(App);
