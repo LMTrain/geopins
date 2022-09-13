99a191f8bfd3aebc11569f49842a80123ea9c07c
LMTrain <layconmm@msn.com>
2022-09-13 18:02:54 -0500

    Creating protected route2

--- client/src/pages/Splash.js
+++ client/src/pages/Splash.js
@@ -1,8 +1,12 @@
-import React from "react";
-import Login from '../components/Auth/Login'
+import React, { useContext } from "react";
+import { Redirect } from 'react-router-dom'
+
+import Login from '../components/Auth/Login';
+import context from '../context';
 
 const Splash = () => {
-  return <Login />;
+  const { state } = useContext(context)
+  return state.isAuth ? <Redirect to="/" /> : <Login />
 };
 
 export default Splash;
