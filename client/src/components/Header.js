bd6c8323c729a0a996cd0aedf7817eef43658a55
LMTrain <layconmm@msn.com>
2022-09-17 22:03:33 -0500

    Placing a Pin at User's Current Position3

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -19,8 +19,8 @@ const Header = ({ classes }) => {
           <div className={classes.grow}>
             <MapIcon className={classes.icon} />
             <Typography
-              component="h1"
-              variant="h6"
+              component="h2"
+              variant="h5"
               color="inherit"
               noWrap
                           
